8da8d189815ad9304e25ce5c1651a768744715eb
Leandro Neves <leandrosoares6@hotmail.com>
2021-06-04 20:05:03 +0000

    feat: OpenAPI settings confugured in entrypoint application

--- src/main/java/com/leandro/santander/SantanderApplication.java
+++ src/main/java/com/leandro/santander/SantanderApplication.java
@@ -2,12 +2,33 @@ package com.leandro.santander;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class SantanderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+			.info(
+				new Info()
+					.title("")
+					.version("1.0")
+					.termsOfService("http://swagger.io/terms")
+					.license(
+						new License()
+						.name("Apache 2.0")
+						.url("http://springdoc.org")
+					)
+			);
 	}
 
 }
